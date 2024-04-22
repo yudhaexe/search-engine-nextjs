5fabfd95e4512c0bb79bfd4644b15295df0eba83
Muhammad Yudha Setiawan <115864291+yudhaexe@users.noreply.github.com>
2024-04-22 22:04:52 +0700

    adjustment : ui search bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@
 
 import { SearchBar } from "@/components/UI/SearchBar";
 
-
-
 export default function Home() {
   const handleSearchSubmit = (query: string) => {
     console.log("Search Query:", query);
   };
 
-  return <SearchBar />;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="text-7xl text-center leading-[85px] my-10 font-bold">
+        Country
+      </div>
+      <div className="px-[25%]  w-full">
+        <SearchBar />
+      </div>
+    </div>
+  );
 }
--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { IconButton, Input } from "@mui/material"; // Adjusted import for Input
+import { IconButton, Input } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 interface SearchBarProps {
@@ -8,13 +8,24 @@ interface SearchBarProps {
 
 export const SearchBar: React.FC<SearchBarProps> = ({}) => {
   return (
-    <div className="relative ">
-      <input
+    <div className="group relative">
+      <Input
         type="text"
-        placeholder="Search..."
-        className="w-full rounded-full border border-gray-300 bg-white py-3 pl-5 pr-12 text-sm focus:border-[#8362F2] focus:outline-none focus:ring-[#8362F2] "
+        placeholder="Type Country Name"
+        className="w-full rounded-lg border border-gray-300 bg-white py-5 pl-5 pr-12 text-sm "
+        sx={{
+          "&.Mui-focused": {
+            borderColor: "#8362F2",
+            outline: "none",
+            ring: "#8362F2",
+          },
+        }}
+        disableUnderline={true}
       />
-      <IconButton type="submit" className="absolute inset-y-0 right-0 p-2">
+      <IconButton
+        type="submit"
+        className="absolute inset-y-0 right-0 p-2 fill-current text-gray-500 group-focus:fill-[#8362F2]"
+      >
         <SearchIcon />
       </IconButton>
     </div>
