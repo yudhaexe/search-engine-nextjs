eec8e1204a0e3ea902def2e605773f9adbbc8556
Muhammad Yudha Setiawan <115864291+yudhaexe@users.noreply.github.com>
2024-04-23 01:36:57 +0700

    feat : search bar api configuration

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@
 import { SearchBar } from "@/components/UI/SearchBar";
 
 export default function Home() {
-  const handleSearchSubmit = (query: string) => {
-    console.log("Search Query:", query);
-  };
-
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <div className="text-7xl text-center leading-[85px] my-10 font-bold">
--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,18 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { IconButton, Input } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import debounce from "lodash/debounce";
+import { Button } from "@mui/base";
 
-interface SearchBarProps {
-  onSubmit: (query: string) => void;
+interface Country {
+  name: {
+    common: string;
+    official: string;
+  };
+  tld: string[];
+  cca2: string;
+  ccn3: string;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({}) => {
+export const SearchBar: React.FC = ({}) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<Country[]>([]);
+  const [error, setError] = useState<string>("");
+
+  const apiUrl: string = `https://restcountries.com/v3.1/name/`;
+
+  const fetchSuggestions = async (query: string): Promise<void> => {
+    if (!query.trim()) {
+      setSuggestions([]);
+      setError("");
+      return;
+    }
+    try {
+      const response = await fetch(`${apiUrl}${encodeURIComponent(query)}`);
+      if (!response.ok) {
+        throw new Error("Response not OK");
+      }
+      const data: Country[] = await response.json();
+      if (data.length === 0) {
+        throw new Error("No data");
+      }
+      setSuggestions(data.slice(0, 5));
+      setError("");
+    } catch (error) {
+      setError("Data Not Found");
+      setSuggestions([]);
+    }
+  };
+
+  const debouncedFetchSuggestions = useCallback(
+    debounce((nextValue: string) => {
+      fetchSuggestions(nextValue);
+    }, 500),
+    []
+  );
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { value } = event.target;
+    setInputValue(value);
+    debouncedFetchSuggestions(value);
+  };
+
   return (
     <div className="group relative">
       <Input
         type="text"
+        value={inputValue}
+        onChange={handleInputChange}
         placeholder="Type Country Name"
-        className="w-full rounded-lg border border-gray-300 bg-white py-5 pl-5 pr-12 text-sm "
+        className="w-full rounded-lg border border-gray-300 bg-white py-5 pl-5 pr-12 text-sm"
         sx={{
           "&.Mui-focused": {
             borderColor: "#8362F2",
@@ -23,11 +77,27 @@ export const SearchBar: React.FC<SearchBarProps> = ({}) => {
         disableUnderline={true}
       />
       <IconButton
-        type="submit"
+        type="button"
         className="absolute inset-y-0 right-0 p-2 fill-current text-gray-500 group-focus:fill-[#8362F2]"
       >
         <SearchIcon />
       </IconButton>
+      {(suggestions.length > 0 || error) && (
+        <div className="absolute w-full bg-white border border-gray-300 rounded-lg z-10 flex flex-col ">
+          {error ? (
+            <li className="p-2 text-red-500">{error}</li>
+          ) : (
+            suggestions.map((country, index) => (
+              <Button
+                key={index}
+                className="px-6 py-4 hover:bg-gray-100 cursor-pointer text-left font-medium text-base leading-5"
+              >
+                {country.name.common}
+              </Button>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
